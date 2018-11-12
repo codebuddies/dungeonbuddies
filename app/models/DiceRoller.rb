eca326370651cb8b22d828df0382020cf26dcdc2
Ty Burns <mooflaghero@gmail.com>
2018-11-11 19:26:10 -0500

    Version 1 of the dice rolling class.
    Using dice notation xdy
    x is the number of dice
    y is the sides of the dice
    
    rollingDiceFunc(x,y) to use the main dice functions
    
    rollD20() function will roll a 1d20 dice.

---
+++ app/models/DiceRoller.rb
@@ -0,0 +1,37 @@
+# This is function takes three inputs and rolls a y sided dice x times storing values in the array.
+# Using simple dice notation  xdy == rollingDiceFunc(x, y, roll)
+# Created by CuDragon please pm me in codebuddies slack with questions.
+
+class DiceRoller
+    @@rolls = Array.new
+
+    def rollingDiceFunc(numberOfDice, diceSize)
+        value = @@rolls
+        if numberOfDice == 1
+            value.push(rand(1..diceSize))
+            #puts "#{value}"  # Testing uncomment to see array Values
+            rolledAmount = value.sum()
+            value.clear
+            return rolledAmount
+        else
+            value.push(rand(1..diceSize))
+            #puts "#{value}" # Testing uncomment to see array Values
+        return rollingDiceFunc(numberOfDice - 1, diceSize)
+        end
+    end
+
+    # This is a function call to run a 1d20 roll
+    # This is the most used dice in our Game Mechanics
+
+    def rollD20()
+        self.rollingDiceFunc(1,20)
+    end
+end
+
+
+# Area for testing the Dice Mechanics 
+
+
+# test = DiceRoller.new()
+# puts test.rollingDiceFunc(4,6)
+# puts test.rollD20()
