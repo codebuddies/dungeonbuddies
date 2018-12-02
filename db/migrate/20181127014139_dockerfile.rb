9ea65ac08fe1c2f113e5dc722b021ba77c8f11d1
Angelo Cordon <angelocordon@gmail.com>
2018-12-02 13:27:41 -0800

    Clean up extra migration files

--- db/migrate/20181127014139_dockerfile.rb
+++
@@ -1,4 +0,0 @@
-class Dockerfile < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20181202152932_create_homes.rb
+++
@@ -1,9 +0,0 @@
-class CreateHomes < ActiveRecord::Migration[5.1]
-  def change
-    create_table :homes do |t|
-      t.string :login
-
-      t.timestamps
-    end
-  end
-end
