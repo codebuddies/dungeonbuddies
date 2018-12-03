26d547588300f53134eea263c2c51a988c0d46b3
Angelo Cordon <angelocordon@gmail.com>
2018-12-02 22:48:22 -0800

    Update sign in view styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'home/login'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root to:  'pages#show', id: 'home'
 end
